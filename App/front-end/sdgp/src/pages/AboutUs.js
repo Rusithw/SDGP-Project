63dcffc2ee2a7c5e7603743447a67d8c384f1127
Dev-mino <devindi.20220431@iit.ac.lk>
2024-01-30 16:18:02 +0530

    Edited the About Us page

--- App/front-end/sdgp/src/pages/AboutUs.js
+++ App/front-end/sdgp/src/pages/AboutUs.js
@@ -21,41 +21,57 @@ function AboutUs() {
       <div className="text">
         <h1><strong> Our Team </strong></h1>
       </div>
-      <div className="team-images width-100">
-        <div className="width-33">
-         <div className="space">
-          <img src="Images/Rusith.jpg" alt="#" />
-          <p> Rusith Wijayanayake <br/> rusith.20221430@iit.ac.lk </p> 
-         </div>
+      <div class="container">
+            <div class="image">
+                <img src="/Images/Rusith.png" alt="#" />
+            </div>
+            <div class="text">
+                <p> Rusith Wijayanayake <br/> rusith.20221430@iit.ac.lk </p> 
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id ducimus deleniti nam perferendis sequi. Veritatis delectus ab neque fugit, distinctio necessitatibus ipsa natus deleniti officiis voluptatum molestiae qui magni dicta.</p>
+            </div>
         </div>
-        <div className="width-33">
-          <div className="space">
-           <img src="Images/about_us_image.jpg" alt="" />
-           <p> Nethmina Fernando <br/> nethmina.20211167@iit.ac.lk </p>
-          </div>
-         </div>
-         <div className="width-33">
-          <div className="space">
-           <img src="Images/about_us_image.jpg" alt="" />
-           <p> Maleesha Shavindi <br/> maleesha.20220977@iit.ac.lk </p>
-          </div>
-         </div>
-         <div className="width-33">
-          <div className="space">
-           <img src="Images/about_us_image.jpg" alt="" />
-           <p> Devindi Fernando <br/> devindi.20220431@iit.ac.lk </p>
-          </div>
-         </div>
-         <div className="width-33">
-          <div className="space">
-           <img src="Images/about_us_image.jpg" alt="" />
-           <p> Lisath Thisurasiri <br/> lisath.20221943@iit.ac.lk </p>
-          </div>
-         
-         </div>
+
+        <div class="container">
+            <div class="image">
+                <img src="/Images/Nethmina.png" alt="#" />
+            </div>
+            <div class="text">
+                <p> Nethmina Fernando <br/> nethmina.20211167@iit.ac.lk </p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id ducimus deleniti nam perferendis sequi. Veritatis delectus ab neque fugit, distinctio necessitatibus ipsa natus deleniti officiis voluptatum molestiae qui magni dicta.</p>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="image">
+                <img src="/Images/Maleesha.png" alt="#" />
+            </div>
+            <div class="text">
+                <p> Maleesha Shavindi <br/> maleesha.20220977@iit.ac.lk </p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id ducimus deleniti nam perferendis sequi. Veritatis delectus ab neque fugit, distinctio necessitatibus ipsa natus deleniti officiis voluptatum molestiae qui magni dicta.</p>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="image">
+                <img src="/Images/Devindi.png" alt="#" />
+            </div>
+            <div class="text">
+                <p> Devindi Fernando <br/> devindi.20220431@iit.ac.lk </p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id ducimus deleniti nam perferendis sequi. Veritatis delectus ab neque fugit, distinctio necessitatibus ipsa natus deleniti officiis voluptatum molestiae qui magni dicta.</p>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="image">
+                <img src="jean.png" alt="#" />
+            </div>
+            <div class="text">
+                <p> Lisath Thisurasiri <br/> lisath.20221943@iit.ac.lk </p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id ducimus deleniti nam perferendis sequi. Veritatis delectus ab neque fugit, distinctio necessitatibus ipsa natus deleniti officiis voluptatum molestiae qui magni dicta.</p>
+            </div>
+        </div>
       </div>
     </div>
-  </div>
 
   )
 }
