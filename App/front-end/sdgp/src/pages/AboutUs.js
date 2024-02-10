01539a45aa3934908ee57be455c8d4e7c542857c
Dev-mino <devindi.20220431@iit.ac.lk>
2024-02-10 21:11:25 +0530

    About us page layout and Styling changed

--- App/front-end/sdgp/src/pages/AboutUs.js
+++ App/front-end/sdgp/src/pages/AboutUs.js
@@ -19,50 +19,73 @@ function AboutUs() {
     <div>
       <NavBar value={"about_us"} />
       <div className="text">
+        <br /><br />
+        
         <h1><strong> Our Team </strong></h1>
       </div>
-      <div class="container">
-            <div class="image">
+      <br /><br />
+        <div class="container1">
+            <div class="image1">
                 <img src="/Images/Rusith.png" alt="#" />
             </div>
-            <div class="text">
-                <p> Rusith Wijayanayake <br/> rusith.20221430@iit.ac.lk </p> 
+            <div class="text1">
+                <p className='text2'> Rusith Wijayanayake <br/> rusith.20221430@iit.ac.lk </p> 
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id ducimus deleniti nam perferendis sequi. Veritatis delectus ab neque fugit, distinctio necessitatibus ipsa natus deleniti officiis voluptatum molestiae qui magni dicta.</p>
             </div>
         </div>
 
-
-
-        <div class="container">
-            <div class="image">
+        <div class="container1">
+            <div class="image1">
                 <img src="/Images/Maleesha.png" alt="#" />
             </div>
-            <div class="text">
-                <p> Maleesha Shavindi <br/> maleesha.20220977@iit.ac.lk </p>
+            <div class="text1">
+                <p className='text2'> Maleesha Shavindi <br/> maleesha.20220977@iit.ac.lk </p>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id ducimus deleniti nam perferendis sequi. Veritatis delectus ab neque fugit, distinctio necessitatibus ipsa natus deleniti officiis voluptatum molestiae qui magni dicta.</p>
             </div>
         </div>
 
-        <div class="container">
-            <div class="image">
-                <img src="/Images/Devindi.png" alt="#" />
-            </div>
-            <div class="text">
-                <p> Devindi Fernando <br/> devindi.20220431@iit.ac.lk </p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id ducimus deleniti nam perferendis sequi. Veritatis delectus ab neque fugit, distinctio necessitatibus ipsa natus deleniti officiis voluptatum molestiae qui magni dicta.</p>
-            </div>
-        </div>
-
-        <div class="container">
-            <div class="image">
+        <div class="container1">
+            <div class="image1">
                 <img src="/Images/Lisath.png" alt="#" />
             </div>
-            <div class="text">
-                <p> Lisath Thisurasiri <br/> lisath.20221943@iit.ac.lk </p>
+            <div class="text1">
+                <p className='text2'> Lisath Thisurasiri <br/> lisath.20221943@iit.ac.lk </p>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id ducimus deleniti nam perferendis sequi. Veritatis delectus ab neque fugit, distinctio necessitatibus ipsa natus deleniti officiis voluptatum molestiae qui magni dicta.</p>
             </div>
         </div>
+        
+        <div class="container1">
+            <div class="image1">
+                <img src="/Images/Devindi.png" alt="#" />
+            </div>
+            <div class="text1">
+                <p className='text2'> Devindi Fernando <br/> devindi.20220431@iit.ac.lk </p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id ducimus deleniti nam perferendis sequi. Veritatis delectus ab neque fugit, distinctio necessitatibus ipsa natus deleniti officiis voluptatum molestiae qui magni dicta.</p>
+            </div>
+        </div>
+
+        <div className='container1'>
+            <img src="/Images/team.png" alt="#" />
+        </div>
+
       </div>
+
+        <br /><br />
+      <div className='container'>
+            <div className='box'>
+                <h2>Why?</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde accusamus, illo doloribus numquam ea dolor dignissimos alias explicabo excepturi placeat!</p>
+            </div>
+            <div className='box'>
+                <h2>How?</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, quo reprehenderit at adipisci temporibus accusantium commodi sunt ratione facere nisi!</p>
+            </div>
+            <div className='box'>
+                <h2>Mission</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa dicta deleniti veritatis dolorum, corporis possimus repudiandae quasi cumque! Atque, porro.</p>
+            </div>
+        </div>
+        <br /><br />
     </div>
 
   )
