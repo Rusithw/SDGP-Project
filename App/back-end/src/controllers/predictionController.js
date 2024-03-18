64b7caae6de27f9453d8e1555dbed1c6520d6aaa
Lisath 20221943 <lisath.20221943@iit.ac.lk>
2024-03-19 02:45:32 +0530

    retrieve programs and modules

--- App/back-end/src/controllers/predictionController.js
+++ App/back-end/src/controllers/predictionController.js
@@ -15,3 +15,26 @@ function queryDatabase(sql, values) {
         });
     });
 }
+
+// Function to retrieve enrolled users with their selected programs and modules
+exports.getEnrolledUsersWithProgramsAndModules = async (userId) => {
+    try {
+        const sql = `SELECT u.user_id,u.user_name,up.university_program_name,um.university_module_name 
+        FROM user u
+      JOIN
+        enrolement e ON u.user_id = e.user_id
+      JOIN
+        university_programs up ON e.university_program_id = up.university_program_id
+      JOIN
+        university_program_modules upm ON e.university_program_id = upm.university_program_id
+      JOIN
+        university_modules um ON upm.university_module_id = um.university_module_id
+      WHERE u.user_id = `+ userId + `
+    `;
+        const results = await queryDatabase(sql);
+        return results;
+    } catch (error) {
+        console.error('Error fetching enrolled users:', error);
+        throw error;
+    }
+}
