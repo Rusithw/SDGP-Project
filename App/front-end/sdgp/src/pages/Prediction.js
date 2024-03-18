7ac9ea892bac2c04481c9b20a8e5ac7735da5504
rusith pansilu <rusith.20221430@iit.ac.lk>
2024-03-19 03:52:05 +0530

    new update on prediction page

--- App/front-end/sdgp/src/pages/Prediction.js
+++ App/front-end/sdgp/src/pages/Prediction.js
@@ -7,13 +7,13 @@ function Prediction() {
   const navigate = useNavigate();
 
   useEffect(() => {
-   const user_session_value =  window.sessionStorage.getItem("user_name");
-   if(user_session_value === null || user_session_value === ""){
-    navigate("/login");
-   }
-   // after user log, user's name will display on the prediction page "user name, please fill your details here"
-   userDetailsByUserName(user_session_value);
-   
+    const user_session_value = window.sessionStorage.getItem("user_name");
+    if (user_session_value === null || user_session_value === "") {
+      navigate("/login");
+    }
+    // after user log, user's name will display on the prediction page "user name, please fill your details here"
+    userDetailsByUserName(user_session_value);
+
 
   }, []);
 
@@ -39,7 +39,7 @@ function Prediction() {
     year4Module5: "",
     year4Module6: "",
 
-    
+
 
   });
 
@@ -49,106 +49,106 @@ function Prediction() {
 
   const [userDetailsByUserName_data, setUserDetailsByUserName] = useState("");
 
-  
-const [year_modules, setYearModules] = useState([]);
 
-   const [first_year_modules, setFirstYearModules] = useState([]);
-   const [second_year_modules, setSecondYearModules] = useState([]);
-   const [third_year_modules, setThirdYearModules] = useState([]);
-   const [fourth_year_modules, setFourthYearModules] = useState([]);
+  const [year_modules, setYearModules] = useState([]);
 
-   const [selected_program, setProgramId] = useState(0);
+  const [first_year_modules, setFirstYearModules] = useState([]);
+  const [second_year_modules, setSecondYearModules] = useState([]);
+  const [third_year_modules, setThirdYearModules] = useState([]);
+  const [fourth_year_modules, setFourthYearModules] = useState([]);
+
+  const [selected_program, setProgramId] = useState(0);
 
   const [pred, setPred] = useState([]);
   const [predProfile, setPredProfile] = useState([]);
-  
-   const handleModuleChange = (event) => {
+
+  const handleModuleChange = (event) => {
     var module = event.target.value;
     var year = event.target.name;
-    
+
     console.log(year)
     console.log(module)
 
-    if(year === "year1"){
-      if(first_year_modules.includes(module)){
+    if (year === "year1") {
+      if (first_year_modules.includes(module)) {
         setFirstYearModules(
           first_year_modules.filter((item) => item !== module)
         );
-      }else{
-        setFirstYearModules([...first_year_modules,module]);
+      } else {
+        setFirstYearModules([...first_year_modules, module]);
       }
     }
-    
-    if(year === "year2"){
-      if(second_year_modules.includes(module)){
+
+    if (year === "year2") {
+      if (second_year_modules.includes(module)) {
         setSecondYearModules(
           second_year_modules.filter((item) => item !== module)
         );
-      }else{
+      } else {
         setSecondYearModules([...second_year_modules, module]);
       }
     }
 
-    if(year === "year3"){
-      if(third_year_modules.includes(module)){
+    if (year === "year3") {
+      if (third_year_modules.includes(module)) {
         setThirdYearModules(
           third_year_modules.filter((item) => item !== module)
         );
-      }else{
+      } else {
         setThirdYearModules([...third_year_modules, module]);
       }
     }
-    
-    if(year === "year4"){
-      if(fourth_year_modules.includes(module)){
+
+    if (year === "year4") {
+      if (fourth_year_modules.includes(module)) {
         setFourthYearModules(
           fourth_year_modules.filter((item) => item !== module)
         );
-      }else{
+      } else {
         setFourthYearModules([...fourth_year_modules, module]);
       }
     }
 
-    
-   }
+
+  }
 
   const handleChange = (event) => {
 
-    const {name, value} = event.target;
+    const { name, value } = event.target;
     setData({
       ...data_set,
       [name]: value,
     });
   };
 
-   // this is a function call user details by user name. this function is used to take the relevant user's name to diaplay in the prediction page
+  // this is a function call user details by user name. this function is used to take the relevant user's name to diaplay in the prediction page
   const userDetailsByUserName = async (user_name) => {
     const payLoad = {
       "user_name": user_name
-     }
-      try {
-        const response = await fetch('http://localhost:3001/api/userDetailsByUserName', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(payLoad),
-        });
-  
-        if (!response.ok) {
-          throw new Error('Invalid');
-        }
-  
-        const data = await response.json();
-        
-        // here, set the relevant user's name
-       setUserDetailsByUserName(data[0].user_first_name);
-        
-     
-      } catch (error) {
-        console.error('Error modules in:', error);
-      
+    }
+    try {
+      const response = await fetch('http://localhost:3001/api/userDetailsByUserName', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payLoad),
+      });
+
+      if (!response.ok) {
+        throw new Error('Invalid');
       }
+
+      const data = await response.json();
+
+      // here, set the relevant user's name
+      setUserDetailsByUserName(data[0].user_first_name);
+
+
+    } catch (error) {
+      console.error('Error modules in:', error);
+
+    }
   }
 
   const handleSubmit = async (event) => {
@@ -213,35 +213,36 @@ const [year_modules, setYearModules] = useState([]);
   };
 
   const programSelect = async (event) => {
+    setProgramId(event.target.value)
     const payLoad = {
-     "university_program_id": event.target.value
+      "university_program_id": event.target.value
     }
-     try {
-       const response = await fetch('http://localhost:3001/api/getModules', {
-         method: 'POST',
-         headers: {
-           'Content-Type': 'application/json',
-         },
-         body: JSON.stringify(payLoad),
-       });
- 
-       if (!response.ok) {
-         throw new Error('Invalid');
-       }
- 
-       const data = await response.json();
-       console.log(data);  
-       setYearModules(data);
-       
-    
-     } catch (error) {
-       console.error('Error modules in:', error);
-     
-     }
-   }
+    try {
+      const response = await fetch('http://localhost:3001/api/getModules', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payLoad),
+      });
+
+      if (!response.ok) {
+        throw new Error('Invalid');
+      }
+
+      const data = await response.json();
+      console.log(data);
+      setYearModules(data);
 
 
-   const [load_Degree_Programs, setLoadDegreePrograms] = useState([]);
+    } catch (error) {
+      console.error('Error modules in:', error);
+
+    }
+  }
+
+
+  const [load_Degree_Programs, setLoadDegreePrograms] = useState([]);
 
   const loadDegreePrograms = async () => {
     try {
@@ -260,33 +261,33 @@ const [year_modules, setYearModules] = useState([]);
 
   return (
     <div className="main">
-      <div> 
-      <NavBar value={"prediction"}/>
-       <div className="width-100">
-        <div className="width-50 prediction-image">
+      <div>
+        <NavBar value={"prediction"} />
+        <div className="width-100">
+          <div className="width-50 prediction-image">
             <div className="prediction-img-content">
-        </div>
-        <a href="#" class="top">&#128743;</a>
-        </div>
-        <div className="width-50">
+              <img src="Images/prediction-image.jpg" alt="#" />
+            </div>
+          </div>
+          <div className="width-50">
             <form onSubmit={handleSubmit}>
-            <div className="detail-form">
+              <div className="detail-form">
                 <div className="nav-padding">
-                  <h2 className='head2'> {userDetailsByUserName_data},  Please fill your details here...</h2>
-                  
-                  <label for="degree-selector">Select you degree program: </label><br /><br />
+                  <h2> {userDetailsByUserName_data}  Please fill your details here</h2>
+
+                  <label for="degree-selector">Select you degree program: </label><br />
                   <select name="degree-program" onChange={programSelect}>
-                  <option value="" >Please select an option</option>
-                  {load_Degree_Programs.map((item) => (
+                    <option value="" >Please select an option</option>
+                    {load_Degree_Programs.map((item) => (
                       <option key={item.university_program_id} value={item.university_program_id}> {item.university_program_name} </option>
-                  ))}
+                    ))}
                   </select>
-                  <br/> <br/><br />
-                  
-                
+                  <br /> <br />
+
+
 
                   <div className="year-module-colour">
-                    <label for="first-year-modules"> <strong> Select your 1st year modules : </strong> </label><br /><br /> 
+                    <label for="first-year-modules">Select your 1st year modules : </label><br />
                     {year_modules.map((item) => (
                       item.year === "1" ? <div key={item.university_program_modules_id} style={{ "width": "100%", "display": "inline-block" }}>
                         <div style={{ "float": "left", "padding": "19px", "min-width": "450px" }}>{item.university_module_name}</div>
@@ -301,9 +302,11 @@ const [year_modules, setYearModules] = useState([]);
                     )
                     )}
 
+
                   </div>
+
                   <div className="year-module-non-colour">
-                    <label for="second-year-modules"> <strong> Select your 2nd year modules: </strong>  </label><br /><br />
+                    <label for="second-year-modules">Select your 2nd year modules: </label><br />
                     {year_modules.map((item) => (
                       item.year === "2" ? <div key={item.university_program_modules_id} style={{ "width": "100%", "display": "inline-block" }}>
                         <div style={{ "float": "left", "padding": "19px", "min-width": "450px" }}>{item.university_module_name}</div>
@@ -320,10 +323,8 @@ const [year_modules, setYearModules] = useState([]);
                     <br /> <br />
                   </div>
 
-                 
-                  
                   <div className="year-module-colour">
-                    <label for="third-year-modules"> <strong> Select your 3rd year modules: </strong> </label><br /><br />
+                    <label for="third-year-modules">Select your 3rd year modules: </label><br />
                     {year_modules.map((item) => (
                       item.year === "3" ? <div key={item.university_program_modules_id} style={{ "width": "100%", "display": "inline-block" }}>
                         <div style={{ "float": "left", "padding": "19px", "min-width": "450px" }}>{item.university_module_name}</div>
@@ -340,10 +341,10 @@ const [year_modules, setYearModules] = useState([]);
                     <br /> <br />
                   </div>
 
-                  
-                  
+
+
                   <div className="year-module-non-colour">
-                    <label for="fourth-year-modules"> <strong> Select your 4th year modules: </strong>  </label><br /><br />
+                    <label for="fourth-year-modules">Select your 4th year modules: </label><br />
                     {year_modules.map((item) => (
                       item.year === "4" ? <div key={item.university_program_modules_id} style={{ "width": "100%", "display": "inline-block" }}>
                         <div style={{ "float": "left", "padding": "19px", "min-width": "450px" }}>{item.university_module_name}</div>
@@ -359,8 +360,8 @@ const [year_modules, setYearModules] = useState([]);
                     )}
                     <br /> <br />
                   </div>
-                  
-                  
+
+
                   <div>
                     <ul>
                       {Object.entries(pred).map(([career, percentage]) => (
